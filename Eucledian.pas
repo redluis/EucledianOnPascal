8894a18f7b2a83e53cf4ee4f1f3dfaf0ef17e8ac
redluis <95424776+redluis@users.noreply.github.com>
2021-12-02 16:13:18 +0200

    v 1.0

---
+++ Eucledian.pas
@@ -0,0 +1,27 @@
+﻿function OriginalEucledianGCD(a, b : integer) : integer;
+begin
+    while (a <> b) do
+    begin
+        (a, b) := (min(a, b), abs(a - b)); 
+    end;
+    Result := a;
+end;
+
+function ModifiedEucledianGCD(a, b : integer) : integer;
+begin
+    if a > b then
+        Swap(a, b);
+        
+    while (b <> 0) do
+    begin    
+       (a, b) := (b, a mod b);
+    end;
+    
+    Result := a;
+end;
+
+Begin
+    var (a, b) := ReadInteger2;
+    Println('Eucledian GCD (original): ', OriginalEucledianGCD(a, b));
+    Println('Eucledian GCD (modified): ', ModifiedEucledianGCD(a, b))
+End.
